1b36a90af3f69f6d223570a12ef703f35848ac67
DorofeevE <legion.0@yandex.ru>
2023-04-19 13:48:15 +0300

    точка входа

--- install.sh
+++ install.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
+sudo apt update
 sudo apt install python3.9
 sudo apt install python3.9-venv
+sudo apt install python3-pip python3-dev build-essential libssl-dev libffi-dev python3-setuptools libpython3.9-dev
 sudo apt install unixodbc-dev
 python3.9 -m venv zas_env
 source zas_env/bin/activate
 pip install -r requirements.txt
-pip install wheel uwsgi
+pip wheel uwsgi
 mv .env.example .env
 python init.py
 curl https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
