bbdc49b97384752eb1df272a932d78eeb0bc8912
DorofeevE <legion.0@yandex.ru>
2023-04-20 15:15:28 +0300

    правки install.sh

--- install.sh
+++ install.sh
@@ -3,8 +3,11 @@
 sudo apt update
 sudo apt install python3.9
 sudo apt install python3.9-venv
-sudo apt install python3-pip python3-dev build-essential libssl-dev libffi-dev python3-setuptools libpython3.9-dev
+sudo apt install python3-pip python3-dev build-essential libssl-dev libffi-dev python3-setuptools
+sudo apt install libpython3.9-dev
 sudo apt install unixodbc-dev
+sudo apt-get install apt-transport-https
+sudo apt-get update
 # настройка окружения
 python3.9 -m venv zas_env
 source zas_env/bin/activate
@@ -12,9 +15,10 @@ pip install -r requirements.txt
 pip install wheel uwsgi
 mv .env.example .env
 python init.py
+deactivate
 # установка драйвера MSSQL
-curl https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
-curl https://packages.microsoft.com/config/ubuntu/$(lsb_release -rs)/prod.list > sudo /etc/apt/sources.list.d/mssql-release.list
+sudo su -c 'curl https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -'
+sudo su -c 'curl https://packages.microsoft.com/config/ubuntu/$(lsb_release -rs)/prod.list > /etc/apt/sources.list.d/mssql-release.list'
 sudo apt-get update
 sudo ACCEPT_EULA=Y apt-get install -y msodbcsql18
 # настройка службы uWSGI
@@ -26,6 +30,7 @@ sed -i "s:WorkingDirectory=.*:WorkingDirectory=$folder:g" ubuntu_deploy/zas.serv
 sed -i "s:Environment=.*:Environment="PATH=$folder'/zas_env/bin'":g" ubuntu_deploy/zas.service
 sed -i "s:ExecStart=.*:ExecStart=$folder/zas_env/bin/uwsgi --ini zas.ini:g" ubuntu_deploy/zas.service
 sudo cp ubuntu_deploy/zas.service /etc/systemd/system/zas.service
+cp $folder/ubuntu_deploy/zas.ini $folder/zas.ini
 sudo systemctl daemon-reload
 sudo systemctl start zas
 sudo systemctl enable zas
