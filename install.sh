635bff3053be5041387db36081c7221314b7017e
DorofeevE <legion.0@yandex.ru>
2023-04-20 16:16:25 +0300

    правка скрипта

--- install.sh
+++ install.sh
@@ -22,7 +22,6 @@ sudo su -c 'curl https://packages.microsoft.com/config/ubuntu/$(lsb_release -rs)
 sudo apt-get update
 sudo ACCEPT_EULA=Y apt-get install -y msodbcsql18
 # настройка службы uWSGI
-cd zabbix_actualization_service
 folder="$PWD"
 user="$USER"
 sed -i "s:User=.*:User=$user:g" ubuntu_deploy/zas.service
@@ -31,6 +30,7 @@ sed -i "s:Environment=.*:Environment="PATH=$folder'/zas_env/bin'":g" ubuntu_depl
 sed -i "s:ExecStart=.*:ExecStart=$folder/zas_env/bin/uwsgi --ini zas.ini:g" ubuntu_deploy/zas.service
 sudo cp ubuntu_deploy/zas.service /etc/systemd/system/zas.service
 cp $folder/ubuntu_deploy/zas.ini $folder/zas.ini
+cp $folder/ubuntu_deploy/wsgi.py $folder/wsgi.py 
 sudo systemctl daemon-reload
 sudo systemctl start zas
 sudo systemctl enable zas
